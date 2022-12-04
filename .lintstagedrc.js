138064b3c6ee9ae296cb930755a73c4a74541b7c
yuechao.xu <yuechao.xu@appshahe.com>
2022-12-04 10:22:58 +0800

    feat: 测试命令 lint 校验是否成功

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   "*.{js,ts,jsx,tsx}": ["eslint --fix"],
+  "*.{vue}": ["stylelint --fix", "eslint --fix"],
   "*.{scss, css, less}": ["stylelint --fix", "prettier --write"],
   "*.{json, md}": ["prettier --write"],
 };
