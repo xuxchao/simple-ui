b3768b92ab0e6c4c4c531328ce4a79530d6d793d
yuechao.xu <yuechao.xu@appshahe.com>
2022-12-25 19:56:43 +0800

    fix: 修复资源找不到的问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "简单UI",
   description: "一个极其简单的组件库",
-  base: "simple-ui",
+  base: "/simple-ui/",
   themeConfig: {
     outline: [2, 3],
     // 右上角的导航
